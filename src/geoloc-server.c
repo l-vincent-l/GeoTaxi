9ca072542c1d53b6797e23e940587875106f5c91
l-vincent-l <lara.vincent@gmail.com>
2015-06-01 10:50:25 +0200

    Add crashing information

--- src/geoloc-server.c
+++ src/geoloc-server.c
@@ -41,28 +41,28 @@ static inline struct sockaddr_in sock_hint (long addr, short port) {
 static inline int parse_msg (struct msg_parts *parts, char *msg, int n) {
    int len=0;
    parts->operator = js0n("operator",0,msg,n,&len);
-   if (0 == len) { return -1; }
+   if (0 == len) { printf("Unable to parse operator"); return -1; }
    parts->operator_len = len;
    parts->taxi = js0n("taxi",0,msg,n,&len);
-   if (0 == len) { return -1; }
+   if (0 == len) { printf("Unable to parse taxi"); return -1; }
    parts->taxi_len = len;
    parts->lat = js0n("lat",0,msg,n,&len);
-   if (0 == len) { return -1; }
+   if (0 == len) { printf("Unable to parse lat"); return -1; }
    parts->lat_len = len;
    parts->lon = js0n("lon",0,msg,n,&len);
-   if (0 == len) { return -1; }
+   if (0 == len) { printf("Unable to parse lon"); return -1; }
    parts->lon_len = len;
    parts->timestamp = js0n("timestamp",0,msg,n,&len);
-   if (0 == len) { return -1; }
+   if (0 == len) { printf("Unable to parse timestamp"); return -1; }
    parts->timestamp_len = len;
    parts->status = js0n("status",0,msg,n,&len);
-   if (0 == len) { return -1; }
+   if (0 == len) { printf("Unable to parse status"); return -1; }
    parts->status_len = len;
    parts->device = js0n("device",0,msg,n,&len);
-   if (0 == len) { return -1; }
+   if (0 == len) { printf("Unable to parse device"); return -1; }
    parts->device_len = len;
    parts->hash = js0n("hash",0,msg,n,&len);
-   if (0 == len) { return -1; }
+   if (0 == len) { printf("Unable to parse hash"); return -1; }
    parts->hash_len = len;
 
    parts->hash[parts->hash_len] = '\0';
